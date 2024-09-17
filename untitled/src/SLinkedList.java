5782c196e8e1600771386014f2e7304a01260327
Sebbak <mehdi.sebbak@etu.univ-amu.fr>
2024-09-17 15:21:40 +0200

    mise en place du projet

---
+++ untitled/src/SLinkedList.java
@@ -0,0 +1,57 @@
+public class SLinkedList {
+	
+	private Node head;
+	private long size;
+	
+	public SLinkedList() {
+		head = null;
+		size = 0;
+	}
+
+	public Node getHead() {
+		return head;
+	}
+	
+	public void setHead(Node head) {
+		this.head = head;
+		if(head != null) {
+			size = 1;
+		}
+	}
+	
+	public Node findPenultimate(Node head) {
+
+		if (head == null || head.getNext() == null) {
+	        return null;
+	    }
+	    
+	    Node current = head;
+	    
+	    while (current.getNext().getNext() != null) {
+	        current = current.getNext();
+	    }
+	    
+	    return current;
+	}
+    public void reverse() {
+        Node previous = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null) {
+            next = current.getNext();
+            current.setNext(previous);
+            previous = current;           
+            current = next;
+        }
+        head = previous;
+    }
+    
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.println(current.getElement());
+            current = current.getNext();
+        }
+    }
+}
---
+++ untitled/src/SLinkedListTest.java
@@ -0,0 +1,33 @@
+public class SLinkedListTest {
+
+    public static void main(String[] args) {
+        SLinkedList list = new SLinkedList();
+        
+        Node node1 = new Node("Premier", null);
+        Node node2 = new Node("Deuxième", null);
+        Node node3 = new Node("Troisième", null);
+        
+        node1.setNext(node2);
+        node2.setNext(node3);
+        
+        list.setHead(node1);
+        
+        Node current = list.getHead();
+        while (current != null) {
+            System.out.println(current.getElement());
+            current = current.getNext();
+        }
+        
+        Node penultimateNode = list.findPenultimate(list.getHead());
+        
+        if (penultimateNode != null) {
+            System.out.println("Avant-dernier élément : " + penultimateNode.getElement());
+        } else {
+            System.out.println("Il n'y a pas d'avant-dernier élément.");
+        }
+        
+        list.reverse();
+        list.printList();
+        
+    }
+}
